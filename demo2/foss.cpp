10aa009572460c92c65752de79c316a4f8521cd0
codxbrexx <mozammilali2018@gmail.com>
2024-10-03 17:22:03 +0530

    foss file demo 2

---
+++ demo2/foss.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+  cout << "Foss wing ";
+    return 0;
+}
--- main.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-  cout << "Hello FOSS Wing!  ";
-  cout << "Update my code ";
-}
